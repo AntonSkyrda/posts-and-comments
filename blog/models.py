57a68ca51c9c658a5e2fd8b5e487b5568e213b15
AntonSkyrda <mr.skyrda@gmail.com>
2024-10-11 00:28:03 +0300

    create post and comment models and register them in admin.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class CreateMessage(models.Model):
+    user_name = models.CharField(max_length=255)
+    user_email = models.EmailField()
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user_name} ---- {self.user_email}"
+
+    class Meta:
+        abstract = True
+
+
+class Post(CreateMessage):
+    pass
+
+
+class Comment(CreateMessage):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
