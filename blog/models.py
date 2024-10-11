fb9f5f4292cc7452062f0f10e52c9a106afbe226
AntonSkyrda <mr.skyrda@gmail.com>
2024-10-12 01:14:20 +0300

    modify comment model to work with comments for comments

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,9 @@ class CreateMessage(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def formatted_date(self):
+        return self.created_at.strftime("%d.%m.%Y %H:%M")
+
     def __str__(self):
         return f"{self.user_name} ---- {self.user_email}"
 
@@ -19,4 +22,11 @@ class Post(CreateMessage):
 
 
 class Comment(CreateMessage):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="replies",
+    )
