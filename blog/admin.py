2dd8be5a21bf5c9bf2a07d71a83cc77844064d1c
AntonSkyrda <mr.skyrda@gmail.com>
2024-10-16 17:06:14 +0300

    update models and add BlogUser model to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Post, Comment
+from .models import Post, Comment, BlogUser
 
 
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(BlogUser)
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,18 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class BlogUser(AbstractUser):
+    pass
+
+
 class CreateMessage(models.Model):
-    user_name = models.CharField(max_length=255)
-    user_email = models.EmailField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="%(class)s_messages",
+    )
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -11,10 +20,11 @@ class CreateMessage(models.Model):
         return self.created_at.strftime("%d.%m.%Y %H:%M")
 
     def __str__(self):
-        return f"{self.user_name} ---- {self.user_email}"
+        return f"{self.created_at} ---- {self.user.username} ---- {self.user.email}"
 
     class Meta:
         abstract = True
+        ordering = ("created_at",)
 
 
 class Post(CreateMessage):
@@ -22,7 +32,11 @@ class Post(CreateMessage):
 
 
 class Comment(CreateMessage):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
     parent = models.ForeignKey(
         "self",
         on_delete=models.CASCADE,
@@ -30,3 +44,8 @@ class Comment(CreateMessage):
         blank=True,
         related_name="replies",
     )
+
+    def save(self, *args, **kwargs):
+        if self.parent and not self.post:
+            self.post = self.parent.post
+        super().save(*args, **kwargs)
