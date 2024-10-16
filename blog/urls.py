930fd73bf7f6549f9a0d4d24614b3a86777390e4
AntonSkyrda <mr.skyrda@gmail.com>
2024-10-16 17:10:04 +0300

    update global urls and blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import (
-    index,
-)
+from .views import index, add_post, BlogUserCreateView, add_comment
 
 urlpatterns = [
     path("", index, name="index"),
+    path("create_posts/", add_post, name="add_post"),
+    path("user/create/", BlogUserCreateView.as_view(), name="user_create"),
+    path("create_comment/<int:post_id>", add_comment, name="add_comment"),
 ]
+
+app_name = "blog"
